9a1202fdc480d8f9a5e36204127df81ed3bbd834
jayanisha <nishajayaraman98@gmail.com>
2022-01-16 19:49:33 +0530

    First commit

---
+++ src/phase1_project/InterfaceFile.java
@@ -0,0 +1,9 @@
+package phase1_project;
+
+public interface InterfaceFile {
+		public void showAllFiles();
+		public void addFile();
+		public void deleteFile();
+		abstract void searchFile();
+	}
+
---
+++ src/phase1_project/Phase_1_project.java
@@ -0,0 +1,84 @@
+package phase1_project;
+import java.io.*;
+import java.util.*;
+
+/*
+ * 
+ * @author Jayanisha
+ *
+ */
+public class Phase_1_project {
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		welcomeScreen();
+		try {
+			menuDriven();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+	public static void welcomeScreen() {
+		System.out.println("Application Name : MAIN APPLICATION \n");
+		System.out.println("Developer Details: Developed by Jayanisha J \n");
+	}
+
+	public static void menuDriven() throws FileNotFoundException {
+		sc = new Scanner(System.in);
+		Project_1_Phase obj = new Project_1_Phase();
+		int option;
+		do {
+		System.out.println("Enter your choice which you want to select: \n");
+		System.out.println("\t1. Retrieve current filenames in ascending order \n");
+		System.out.println("\t2. Business-level operation menu \n");
+		System.out.println("\t3. Exit from the application \n");
+
+		option=sc.nextInt();
+		switch(option) {
+		case 1:
+			obj.showAllFiles();
+			break;
+		case 2:
+			int ch;
+		do {
+			System.out.println("Enter your choice for Business level operation");
+			System.out.println("\t1. Add a file and its content to a directory");
+			System.out.println("\t2. Delete a file from a directory");
+			System.out.println("\t3. Searching a file and showing its content");
+			System.out.println("\t4. Exit from BOL menu");
+
+			ch=sc.nextInt();
+			switch(ch) {
+			case 1:
+				obj.addFile();
+				break;
+			case 2:
+				obj.deleteFile();
+				break;
+			case 3:
+				obj.searchFile();
+				break;
+			case 4:
+				System.out.println("Exited from the Project_1_Phase...");
+				System.out.println("---------------------------------\n");
+				break;
+			default:
+				System.out.println("Invalid Choice");
+				break;
+			}
+		}while(ch!=4);
+		break;
+		case 3:
+			System.out.println("Exiting from the application...");
+			break;
+		default:
+			System.out.println("Invalid choice");
+		}
+
+		//System.out.println("Press 0 to continue");
+		}while(option!=3);
+
+	}
+}
